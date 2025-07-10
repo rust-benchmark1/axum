53e60be0a6e2708eae27a805714475590cf3e7e1
rust-benchmark1 <rust-benchmark@proton.me>
2025-07-10 12:53:17 -0300

    CWE 22 Update file_writer.rs

--- axum-extra/src/response/file_writer.rs
+++ axum-extra/src/response/file_writer.rs
@@ -8,11 +8,11 @@ pub fn write_file_content(file_path: &str, content: &[u8]) -> io::Result<()> {
 }
 
 pub fn open_file_for_writing(file_path: &str) -> io::Result<fs::File> {
-    //SINK
     let file = OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
+        //SINK
         .open(file_path)?;
     Ok(file)
 } 
